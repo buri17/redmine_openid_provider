f330aa43fe558116345429b6dbddd919be3bddae
Kenji Takahashi <takahashi@noex-inc.co.jp>
2016-08-30 03:11:35 +0000

    Rails 4

--- config/routes.rb
+++ config/routes.rb
@@ -1,18 +1,18 @@
 # Plugin's routes
 # See: http://guides.rubyonrails.org/routing.html
 
-match 'openid', to: 'open_id_provider#checkid_setup',
+post 'openid', to: 'open_id_provider#checkid_setup',
   constraints: lambda { |request|
-    request.query_parameters["openid.mode"] == "checkid_setup" }
+    request.request_parameters["openid.mode"] == "checkid_setup" }
 
-match 'openid', to: 'open_id_provider#checkid_immediate',
+post 'openid', to: 'open_id_provider#checkid_immediate',
   constraints: lambda { |request|
-    request.query_parameters["openid.mode"] == "checkid_immediate" }
+    request.request_parameters["openid.mode"] == "checkid_immediate" }
 
-match 'openid', to: 'open_id_provider#handle_direct_request', via: 'POST'
+post 'openid', to: 'open_id_provider#handle_direct_request'
 
-match 'openid', :to => 'open_id_provider#index'
-match 'openid/xrds', :to => 'open_id_provider#idp_xrds', :defaults => { :format => 'xrds' }
-match 'openid/confirm', :to => 'open_id_provider#confirm'
-match 'openid/user/:id', :to => 'open_id_provider#user_page'
-match 'openid/user/:id/xrds', :to => 'open_id_provider#user_xrds', :defaults => { :format => 'xrds' }
+get 'openid', :to => 'open_id_provider#index'
+get 'openid/xrds', :to => 'open_id_provider#idp_xrds', :defaults => { :format => 'xrds' }
+post 'openid/confirm', :to => 'open_id_provider#confirm'
+get 'openid/user/:id', :to => 'open_id_provider#user_page'
+get 'openid/user/:id/xrds', :to => 'open_id_provider#user_xrds', :defaults => { :format => 'xrds' }
